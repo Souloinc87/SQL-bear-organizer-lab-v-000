e6379704957601803d41ed9fa0f0f5e20806d5f2
Marcus  = Soulo <souloinc87@gmail.com>
2018-08-22 18:43:19 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate",
 1, "male", "brown", "angry", "Y")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2,
 "male", "red", "nice", "Y")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha",
+5, "female", "orange", "loving", "Y")
